e34c825b702086e392803a42d8b0c5a03778a9cc
Anton Kashirov <anton.kashirov@gmail.com>
2024-12-30 22:25:44 +0300

    feat: add metrics module (#11)

---
+++ src/metrics-api/metrics-api.module.ts
@@ -0,0 +1,17 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@revisium/core';
+import * as client from 'prom-client';
+import { MetricsEnabledGuard } from 'src/metrics-api/metrics-enabled.guard';
+import { MetricsController } from 'src/metrics-api/metrics.controller';
+
+@Module({
+  imports: [ConfigModule, DatabaseModule],
+  providers: [MetricsEnabledGuard],
+  controllers: [MetricsController],
+})
+export class MetricsApiModule implements OnModuleInit {
+  onModuleInit() {
+    client.collectDefaultMetrics();
+  }
+}
