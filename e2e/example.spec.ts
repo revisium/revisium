e8ee0b18b35adfdf8b297a97ff0e12d3e681d6c9
anton62k <anton.kashirov@gmail.com>
2025-05-06 08:14:54 +0300

    updated tests

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,20 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://revisium.io/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/revisium/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Try Revisium Cloud (Alpha)', async ({ page }) => {
+  await page.goto('https://revisium.io/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // Click the cloud link.
+  await page.getByText('Try Revisium Cloud (Alpha)').click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole('heading', { name: 'Installation' }),
-  ).toBeVisible();
+  expect(page).toHaveURL(`https://cloud.revisium.io/`);
 });
